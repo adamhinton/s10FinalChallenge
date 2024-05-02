49343ee40534967e0da563d3692334db8d9245a8
adamhinton <adam.d.hinton@gmail.com>
2024-05-02 13:41:11 -0400

    Starting final project

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>Hello final project world</View>
+    </SafeAreaView>;
+  );
+};
+
+export default App;
